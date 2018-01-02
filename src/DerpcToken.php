034ebc5106e6d75d9e618f154d4726458451886d
vagrant <vagrant@homestead>
2018-01-02 07:47:33 +0000

    init

---
+++ src/DerpcToken.php
@@ -0,0 +1,48 @@
+<?php
+
+
+class DerpcToken
+{
+    private $aServerList = [];
+
+    protected function __construct()
+    {
+        $this->aServerList = require_once "./server.php";
+    }
+
+    public function createToken( $sRole, $sInterfaceName, $aData = [] )
+    {
+        if( empty( $sRole ) || empty( $sInterfaceName ) ){
+            return false;
+        }
+
+        $sToken = $this->_createToken( $sRole, $sInterfaceName, $aData );
+
+    }
+
+    public function verifyToken( $sToken, &$aData )
+    {
+        $bRet = false;
+        return $bRet;
+
+    }
+
+    public function setConfig( $sRole, array $aServerList )
+    {
+
+    }
+
+
+
+    private function _createToken( $sRole, $sInterfaceName, $aData )
+    {
+        $aHeader = [
+            'typ'=> 'JWT',
+            'alg'=> 'HS256'
+        ];
+        dd(hash_hmac('sha256',$aHeader,'111'));
+    }
+
+
+
+}
